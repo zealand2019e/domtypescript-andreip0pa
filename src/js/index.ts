dcbbd2c3e24965dfb2e3bd9ebef7dc52f418b5ad
Andrei Popa <andreip0pa44@gmail.com>
2019-10-02 14:42:33 +0200

    Random

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,25 @@
 //Her skal du skrive din typescriptkode
+let element0:HTMLElement=<HTMLElement>document.getElementById("0");
+element0.innerText="Sukkerfri yoghurt";
+if (element0.hasAttribute("class"))
+
+{
+console.log("element has class");
+(element0.setAttribute("class","sundt"));
+
+
+}
+
+let elementList1:HTMLCollectionOf<HTMLElement>=<HTMLCollectionOf<HTMLElement>>document.getElementsByClassName("sundt");
+for (let i =0; i<elementList1.length;i++){
+elementList1[i].style.backgroundColor="lightblue";
+elementList1[i].style.color="black";
+if (i%2==0)
+elementList1[i].style.borderRadius="";
+if (i%2==1)
+elementList1[i].style.borderRadius="132px";
+}
+
+
+//let elementList2:HTMLCollectionOf
+
